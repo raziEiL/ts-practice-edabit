3a3f14d0ece9eec034f07d56df9c96e872afd74e
Denis Medvedev <mr.raz4291@gmail.com>
2020-03-14 11:24:49 +0500

    Added jest

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,58 +1,126 @@
 import * as Main from "./main";
 
 describe("Edabit Challenges", () => {
-    /*     test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });
-        test("", () => {
-    
-        });*/
+    test("isJS", () => {
+        expect(Main.isJS("/users/user.jsx")).toBeTruthy();
+        expect(Main.isJS("/users/user.js")).toBeTruthy();
+        expect(Main.isJS("/users/user.ts")).toBeTruthy();
+        expect(Main.isJS("/users/user.jsxx")).toBeFalsy();
+        expect(Main.isJS("/users/user js")).toBeFalsy();
+    });
+    test("addition", () => {
+        expect(Main.addition(2)).toBe(3);
+        expect(Main.addition(-9)).toBe(-8);
+    });
+    test("getFirstValue", () => {
+        expect(Main.getFirstValue([1, 2, 3])).toEqual([1, 1, 1]);
+        expect(Main.getFirstValue([3, 2, 1])).toEqual([3, 3, 3]);
+    });
+    test("isFourLetters", () => {
+        expect(Main.isFourLetters(["Ryan", "Kieran", "Jason", "Matt"])).toEqual(["Ryan", "Matt"]);
+    });
+    test("googlify", () => {
+        expect(Main.googlify(0)).toBe("invalid");
+        expect(Main.googlify(3)).toBe("Gooogle");
+    });
+    test("wordBuilder", () => {
+        expect(Main.wordBuilder(["e", "t", "s", "t"], [1, 3, 2, 0])).toBe("test");
+        expect(Main.wordBuilder(["b", "e", "t", "i", "d", "a"], [3, 0, 5, 4, 1, 2])).toBe("edabit");
+    });
+    test("reverse", () => {
+        expect(Main.reverse("Think different.")).toBe(".tnereffid knihT");
+    });
+    test("Go", () => {
+        expect(Main.Go(2)).toBe("--");
+        expect(Main.Go(5)).toBe("-----");
+    });
+    test("countWords", () => {
+        expect(Main.countWords("It's high noon")).toBe(3);
+        expect(Main.countWords("Is this easy mode")).toBe(4);
+        expect(Main.countWords("Just an example here move along")).toBe(6);
+        expect(Main.countWords("How are you today?")).toBe(4);
+    });
+    test("countWordsB", () => {
+        expect(Main.countWordsB("This is a test")).toBe(4);
+        expect(Main.countWordsB("What an easy task, right")).toBe(5);
+    });
+    test("findLargestNum", () => {
+        expect(Main.findLargestNum([4, 5, 1, 3])).toBe(5);
+    });
+    test("sevenBoom", () => {
+        expect(Main.sevenBoom([1, 2, 3, 4, 5, 6, 7])).toBe("Boom!");
+        expect(Main.sevenBoom([8, 6, 33, 100])).toBe("there is no 7 in the array");
+    });
+    test("firstLast", () => {
+        expect(Main.firstLast([5, 10, 15, 20, 25])).toEqual([5, 25]);
+        expect(Main.firstLast(["edabit", 13, null, false, true])).toEqual(["edabit", true]);
+    });
+    test("noOdds", () => {
+        expect(Main.noOdds([1, 2, 3, 4, 5, 6, 7, 8])).toEqual([2, 4, 6, 8]);
+        expect(Main.noOdds([43, 65, 23, 89, 53, 9, 6])).toEqual([6]);
+    });
+    test("combinations", () => {
+        expect(Main.combinations(52, 52)).toBe(1);
+        expect(Main.combinations(5, 52)).toBe(2598960);
+    });
+    test("f (Factorial)", () => {
+        expect(Main.f(5)).toBe(120);
+        expect(Main.f(10)).toBe(3628800);
+    });
+    test("missingAngle", () => {
+        expect(Main.missingAngle(27, 59)).toBe("obtuse");
+        expect(Main.missingAngle(135, 11)).toBe("acute");
+        expect(Main.missingAngle(45, 45)).toBe("right");
+    });
+    test("profit", () => {
+        expect(Main.profit({
+            costPrice: 32.67,
+            sellPrice: 45.00,
+            inventory: 1200
+        })).toBe(14796);
+        expect(Main.profit({
+            costPrice: 225.89,
+            sellPrice: 550.00,
+            inventory: 100
+        })).toBe(32411);
+        expect(Main.profit({
+            costPrice: 2.77,
+            sellPrice: 7.95,
+            inventory: 8500
+        })).toBe(44030);
+    });
+    test("validColor RGB", () => {
+        expect(Main.validColor("rgb(100%,100%,101%)")).toBeFalsy();
+        expect(Main.validColor("rgb(0%,0,0)")).toBeTruthy();
+        expect(Main.validColor("rgb(0,,0)")).toBeFalsy();
+        expect(Main.validColor("rgb( 255, 256  ,255 )")).toBeFalsy();
+        expect(Main.validColor("rgba(0,0,0,0.123456789)")).toBeTruthy();
+    });
+    test("additiveInverse", () => {
+        expect(Main.additiveInverse([5, -7, 8, 3])).toEqual([-5, 7, -8, -3]);
+        expect(Main.additiveInverse([1, 1, 1, 1, 1])).toEqual([-1, -1, -1, -1, -1]);
+        expect(Main.additiveInverse([-5, -25, 35])).toEqual([5, 25, -35]);
+    });
+    test("unlucky13", () => {
+        expect(Main.unlucky13([53, 182, 435, 591, 637])).toEqual([53, 435, 591]);
+        expect(Main.unlucky13([24, 316, 393, 458, 1279])).toEqual([24, 316, 393, 458, 1279]);
+        expect(Main.unlucky13([104, 351, 455, 806, 871])).toEqual([]);
+    });
     test("addEnding", () => {
         expect(Main.addEnding(["clever", "meek", "hurried", "nice"], "ly")).toEqual(["cleverly", "meekly", "hurriedly", "nicely"]);
         expect(Main.addEnding(["new", "pander", "scoop"], "er")).toEqual(["newer", "panderer", "scooper"]);
         expect(Main.addEnding(["bend", "sharpen", "mean"], "ing")).toEqual(["bending", "sharpening", "meaning"]);
     });
+    test("Employee class", () => {
+        expect(new Main.Employee("John", "Smith").fullname).toBe("John Smith");
+        expect(new Main.Employee("Mary", "Sue").email).toBe("mary.sue@company.com");
+        expect(new Main.Employee("Antony", "Walker").firstname).toBe("Antony");
+    });
     test("reverse boolean", () => {
         expect(Main.reverseB(true)).toBeFalsy();
         expect(Main.reverseB(false)).toBeTruthy();
-        expect(Main.reverseB(0)).toEqual("boolean expected");
-        expect(Main.reverseB(null)).toEqual("boolean expected");
+        expect(Main.reverseB(0)).toBe("boolean expected");
+        expect(Main.reverseB(null)).toBe("boolean expected");
     });
     test("multiplyByLength", () => {
         expect(Main.multiplyByLength([2, 3, 1, 0])).toEqual([8, 12, 4, 0]);
@@ -72,5 +140,21 @@ describe("Edabit Challenges", () => {
         expect(Main.pianoKeys(71)).toEqual(["G", "6", "White", "1567.98"]);
         expect(Main.pianoKeys(16)).toEqual(["C", "2", "White", "65.41"]);
         expect(Main.pianoKeys(29)).toEqual(["C♯/D♭", "3", "Black", "138.59"]);
+    });
+    /*     test("", () => {
+        expect().toEqual();
+    });*/
+    test("additivePersistence", () => {
+        expect(Main.additivePersistence(5)).toBe(0);
+        expect(Main.additivePersistence(27)).toBe(1);
+        expect(Main.additivePersistence(58)).toBe(2);
+        expect(Main.additivePersistence(5789)).toBe(3);
+    });
+    test("multiplicativePersistence", () => {
+        expect(Main.multiplicativePersistence(7)).toBe(0);
+        expect(Main.multiplicativePersistence(1234567890)).toBe(1);
+        expect(Main.multiplicativePersistence(39)).toBe(3);
+        expect(Main.multiplicativePersistence(6788)).toBe(6);
+        expect(Main.multiplicativePersistence(277777788888899)).toBe(11);
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,131 +1,76 @@
 // #1
-function isJS(path: string) {
-    console.log(/\.(jsx|js|ts)$/.test(path));
+export function isJS(path: string) {
+    return /\.(jsx|js|ts)$/.test(path);
 }
-
-isJS("/users/user.jsx");
-isJS("/users/user.js");
-isJS("/users/user.ts");
-isJS("/users/user.jsxx");
-isJS("/users/user js");
 
 // #2
-const addition = (num: number) => ++num;
-
-addition(2);
-addition(-9);
+export const addition = (num: number) => ++num;
 
 // #3
-function getFirstValue(arr: Array<number>) {
-    return arr.shift();
-}
-
-function getFirstValueB(arr: Array<number>) {
-    return arr[0];
-}
-
-function getFirstValueC(arr: Array<number>) {
+export function getFirstValue(arr: Array<number>) {
     let a: any;
     [a] = arr;
-    return a;
+    return [a, arr[0], arr.shift()];
 }
-
-console.log(getFirstValue([1, 2, 3]));
-console.log(getFirstValueB([1, 2, 3]));
-console.log(getFirstValueC([1, 2, 3]));
 
 // #4
-const isFourLetters = (arr: string[]) => arr.filter((str) => str.length == 4);
-console.log(isFourLetters(["Ryan", "Kieran", "Jason", "Matt"]));
+export const isFourLetters = (arr: string[]) => arr.filter((str) => str.length == 4);
 
 // #5
-function googlify(n: number) {
-    if (n < 2)
-        return "invalid";
-    let o = "";
-    while (n--)
-        o += "o";
-    return "G" + o + "gle";
-}
-
-function googlifyB(n: number) {
+export function googlify(n: number) {
     return n > 1 ? "G" + "o".repeat(n) + "gle" : "invalid";
 }
 
-console.log(googlify(3));
-console.log(googlifyB(3));
-
 // #6 https://edabit.com/challenge/PMKNyPeYgz7weyNXF
-function wordBuilder(letters: string[], positions: number[]) {
+export function wordBuilder(letters: string[], positions: number[]) {
     const word: string[] = [];
     letters.forEach((letter, index) => word[positions[index]] = letter);
     return word.join("");
 }
 
-console.log(wordBuilder(["e", "t", "s", "t"], [1, 3, 2, 0]));
-console.log(wordBuilder(["g", "e", "o"], [1, 0, 2]));
-
 // #7 https://edabit.com/challenge/oS3wCwLWsDqvs7XKd
-function reverse(str: string) {
+export function reverse(str: string) {
     return Array.from(str).reverse().join("");
 }
 
-console.log(reverse("Hello World"));
-
 // #8 https://edabit.com/challenge/3cBe8af7yG2rzLEA2
-function Go(num: number) {
+export function Go(num: number) {
     return "-".repeat(num);
 }
 
-console.log(Go(5));
-
 // #9 https://edabit.com/challenge/FydPqxRiosP7EBiQh
-function countWords(str: string) {
+export function countWords(str: string) {
     const word = str.match(/\s/g);
     return word ? word.length + 1 : 1;
 }
 
-function countWordsB(str: string) {
+export function countWordsB(str: string) {
     return str.split(" ").length;
 }
 
-console.log(countWords("This is a test"));
-console.log(countWordsB("What an easy task, right"));
-
 // #10 Find larges num https://edabit.com/challenge/HTbRYCmPwfZGnBcxW
-function findLargestNum(arr: number[]) {
+export function findLargestNum(arr: number[]) {
     return arr.sort((a, b) => b - a)[0];
 }
 
-console.log(findLargestNum([4, 5, 1, 3]));
-
 // #11 https://edabit.com/challenge/6R6gReGTGwzpwuffD
-function sevenBoom(arr: number[]) {
+export function sevenBoom(arr: number[]) {
     return arr.join("").search("7") != -1 ? "Boom!" : "there is no 7 in the array";
 }
 
-console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
-console.log(sevenBoom([8, 6, 33, 100]));
-
 // #12 https://edabit.com/challenge/R3AYf3eTdATXTQTdZ
-function firstLast(arr: any[]) {
+export function firstLast(arr: any[]) {
     return [arr[0], arr[arr.length - 1]];
 }
 
-console.log(firstLast([5, 10, 15, 20, 25])); // [5, 25]
-console.log(firstLast(["edabit", 13, null, false, true])); // ["edabit", true]
-
 // #13 https://edabit.com/challenge/uJ9K3HtbjHoDGXKhg
-function noOdds(arr: number[]) {
+export function noOdds(arr: number[]) {
     return arr.filter(n => n % 2 === 0);
 }
 
-console.log(noOdds([1, 2, 3, 4, 5, 6, 7, 8])); // [2, 4, 6, 8]
-console.log(noOdds([43, 65, 23, 89, 53, 9, 6])); // [6]
-
 // #14 https://edabit.com/challenge/Wk7ScSpAG4wLSqm8k
 // Формула числа сочетаний: n!/(n−k)!⋅k!
-function combinations(k: number, n: number) {
+export function combinations(k: number, n: number) {
     // 5! = 1*2*3*4*5=120
     function factorial(n: number) {
         let f = 1;
@@ -136,20 +81,13 @@ function combinations(k: number, n: number) {
     return Math.round(factorial(n) / (factorial(n - k) * factorial(k)));
 }
 
-console.log(combinations(52, 52)); // 1
-console.log(combinations(5, 52)); // 2598960
-
-const f = (n: number): number => n <= 1 ? 1 : n * f(n - 1); // факториал числа 5! = 1*2*3*4*5=120
+export const f = (n: number): number => n <= 1 ? 1 : n * f(n - 1); // факториал числа 5! = 1*2*3*4*5=120
 
 // #15 https://edabit.com/challenge/djyXcJZAuezrSXJ4j
-function missingAngle(angle1: number, angle2: number) {
+export function missingAngle(angle1: number, angle2: number) {
     const angle3 = 180 - angle1 - angle2;
     return angle3 === 90 ? "right" : angle3 < 90 ? "acute" : "obtuse";
 }
-
-console.log(missingAngle(27, 59)); // "obtuse"
-console.log(missingAngle(135, 11)); // "acute"
-console.log(missingAngle(45, 45)); // "right"
 
 // #16 https://edabit.com/challenge/GJn7xcBiCLdCNXFgy
 interface Info {
@@ -158,30 +96,12 @@ interface Info {
     inventory: number;
 }
 
-function profit(info: Info) {
+export function profit(info: Info) {
     return Math.round(info.sellPrice * info.inventory - info.costPrice * info.inventory);
 }
 
-console.log(profit({
-    costPrice: 32.67,
-    sellPrice: 45.00,
-    inventory: 1200
-})); // 14796
-
-console.log(profit({
-    costPrice: 225.89,
-    sellPrice: 550.00,
-    inventory: 100
-})); // 32411
-
-console.log(profit({
-    costPrice: 2.77,
-    sellPrice: 7.95,
-    inventory: 8500
-})); // 44030
-
 // #17 RGB check https://edabit.com/challenge/CoAPnr8jxcTah2CzZ
-function validColor(color: string) {
+export function validColor(color: string) {
     const regex = color.match(/(rgb|rgba)\((.*?)\)/);
     if (regex) {
         const arr = regex[2].split(","); // [ '0', '', '0' ]
@@ -203,30 +123,15 @@ function validColor(color: string) {
     return false;
 }
 
-console.log("#17");
-console.log(validColor("rgb(100%,100%,101%)")); //  ➞ false
-console.log(validColor("rgb(0%,0,0)")); // ➞ true
-console.log(validColor("rgb(0,,0)")); // ➞ false
-console.log(validColor("rgb( 255, 256  ,255 )")); // ➞ false
-console.log(validColor("rgba(0,0,0,0.123456789)")); // ➞ true
-
-
 // #18 Additive Inverse https://edabit.com/challenge/Qbi3GuXjwW5fsoKRn
-function additiveInverse(arr: number[]) {
+export function additiveInverse(arr: number[]) {
     return arr.map(n => -n);
 }
-console.log(additiveInverse([5, -7, 8, 3])); // ➞[-5, 7, -8, -3]
-console.log(additiveInverse([1, 1, 1, 1, 1])); // ➞[-1, -1, -1, -1, -1]
-console.log(additiveInverse([-5, -25, 35])); // ➞[5, 25, -35]
 
 // #19 Unlucky 13 https://edabit.com/challenge/5232asan2yFuMGnHq
-function unlucky13(nums: number[]) {
+export function unlucky13(nums: number[]) {
     return nums.filter(n => n % 13);
 }
-
-console.log(unlucky13([53, 182, 435, 591, 637])); // ➞ [53, 435, 591]
-console.log(unlucky13([24, 316, 393, 458, 1279])); // ➞ [24, 316, 393, 458, 1279]
-console.log(unlucky13([104, 351, 455, 806, 871])); // ➞ []
 
 // #20 Word Endings https://edabit.com/challenge/hwWLHcDoPZmXcGHyd
 export function addEnding(arr: string[], ending: string) {
@@ -234,7 +139,7 @@ export function addEnding(arr: string[], ending: string) {
 }
 
 // #21 https://edabit.com/challenge/kGLhgwGaLJsCMS7wS
-class Employee {
+export class Employee {
     firstname: string;
     lastname: string;
     email: string;
@@ -246,15 +151,6 @@ class Employee {
         this.fullname = `${firstname} ${lastname}`;
     }
 }
-
-let emp1 = new Employee("John", "Smith");
-console.log(emp1.fullname); // ➞ "John Smith"
-
-emp1 = new Employee("Mary", "Sue");
-console.log(emp1.email); // ➞ "mary.sue@company.com"
-
-emp1 = new Employee("Antony", "Walker");
-console.log(emp1.firstname); // ➞ "Antony"
 
 // #22 Boolean check
 export function reverseB(bool: any) {
@@ -275,4 +171,37 @@ export function pianoKeys(key: number) {
     key -= 1;
     const index = key % 12;
     return [NOTES[index], key < 3 ? "0" : Math.trunc((key + 9 /* offset */) / 12).toString(), NOTES[index].length > 1 ? "Black" : "White", noteFreq(key)];
+}
+
+// #25 https://edabit.com/challenge/vHvu4Wis8RhmQbwXm
+// 1 + 6 + 7 + 9 + 5 + 8 + 3 = 39
+// 3 + 9 = 12
+// 1 + 2 = 3
+// It takes 3 iterations to reach a single-digit number.
+export function additivePersistence(n: number) {
+    return persistence(n, (p, c) => p + c);
+}
+
+// 7 x 7 = 49
+// 4 x 9 = 36
+// 3 x 6 = 18
+// 1 x 8 = 8
+// It takes 4 iterations to reach a single-digit number.
+export function multiplicativePersistence(n: number) {
+    return persistence(n, (p, c) => p * c);
+}
+
+type ReduceCbk = (previousValue: number, currentValue: number, currentIndex: number, array: number[]) => number;
+
+function persistence(n: number, f: ReduceCbk) {
+    if (n < 10)
+        return 0;
+    function recursiveSum(n: number, count: number = 0): number {
+        count++;
+        const sum = n.toString().split("").map(n => parseInt(n)).reduce(f);
+        if (sum > 9)
+            return recursiveSum(sum, count);
+        return count;
+    }
+    return recursiveSum(n);
 }
